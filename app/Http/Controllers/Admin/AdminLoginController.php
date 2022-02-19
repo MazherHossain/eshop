bf83f22ddd598f36fa361bf28a305593a9c3fd41
Syed Mazher Hossain Zaidi <hossainmazher@gmail.com>
2022-02-20 00:46:54 +0600

    super admin access granted

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
     //show admin login form
     public function showAdminLoginForm(){
         return view('admin.login');
+    }
+/**
+ * Admin Login
+*/
+    public function AdminLoginSystem(Request $request){
+        
+        if(Auth::guard('admin')-> attempt([ 'email'=> $request->email,'password'=> $request -> password ])){
+            return redirect() -> route('admin.dashboard');
+        }
+        else{
+            return redirect() -> route('admin.login.form');
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -7,5 +9,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin/dashboard', 'App\Http\Controllers\Admin\AdminDashboardController@adminDashboardShow');
-Route::get('admin/login', 'App\Http\Controllers\Admin\AdminLoginController@showAdminLoginForm');
+//Route::get('admin/dashboard', 'App\Http\Controllers\Admin\AdminDashboardController@adminDashboardShow'); //old system
+Route::get ('admin/dashboard', [AdminDashboardController::class, 'adminDashboardShow']) -> middleware('admin') ->name('admin.dashboard'); //laravel 9 new method
+//Route::get('admin/login', 'App\Http\Controllers\Admin\AdminLoginController@showAdminLoginForm'); //old method
+Route::get ('admin/login', [AdminLoginController::class, 'showAdminLoginForm']) ->name('admin.login.form'); //laravel 9 new method
+Route::post ('admin/login', [AdminLoginController::class, 'AdminLoginSystem']) ->name('admin.login');
